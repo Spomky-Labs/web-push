7dfec0538e04341736a6a45fa39bef5e5102de59
Spomky <contact@spomky-labs.com>
2021-11-23 13:51:32 +0100

    ECS fix

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use Symfony\Component\Filesystem\Filesystem;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Filesystem())->remove([__DIR__ . '/../var/cache/test']);
 
