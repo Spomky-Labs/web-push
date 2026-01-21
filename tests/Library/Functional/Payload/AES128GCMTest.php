536ebaf277e03e5a7edce5ec7c265672d0fb7279
Florent Morselli <florent.morselli@spomky-labs.com>
2026-01-21 14:58:51 +0100

    Update composer.json to support Symfony 8.0 (#64)
    * Update composer.json to support Symfony 8.0
    
    * Refactor imports for consistency and clarity across multiple files

--- tests/Library/Functional/Payload/AES128GCMTest.php
+++ tests/Library/Functional/Payload/AES128GCMTest.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace WebPush\Tests\Library\Functional\Payload;
 
+use function chr;
+use function count;
 use InvalidArgumentException;
+use function openssl_decrypt;
+use const OPENSSL_RAW_DATA;
+use function ord;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use function preg_match;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\NullAdapter;
 use Symfony\Component\Clock\NativeClock;
+use function unpack;
 use WebPush\Base64Url;
 use WebPush\Exception\OperationException;
 use WebPush\Payload\AES128GCM;
@@ -20,13 +27,6 @@ use WebPush\Payload\ServerKey;
 use WebPush\RequestData;
 use WebPush\Subscription;
 use WebPush\Utils;
-use function chr;
-use function count;
-use function openssl_decrypt;
-use function ord;
-use function preg_match;
-use function unpack;
-use const OPENSSL_RAW_DATA;
 
 /**
  * @internal
